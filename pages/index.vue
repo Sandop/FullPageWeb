d7f0671d92d11153b1cd45bfacf180b8e9a84e7a
Sandop <dpjsdu@163.com>
2019-04-17 16:56:29 +0800

    test

--- pages/index.vue
+++ pages/index.vue
@@ -21,7 +21,7 @@
 							<ul>
 								<li class="list" v-for=" (list,index) in pageTwoCons" :key="index">
 									<page-two-list :pageTwoCon="list"></page-two-list>
-								</li>11
+								</li>
 							</ul>
 						</div>
 					</div>
